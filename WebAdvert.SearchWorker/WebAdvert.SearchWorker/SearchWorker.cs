1fb560df71c75ab2ce8c95464a0ea99c7ff0886b
Allan Egidio <allan.egidio@outlook.com>
2021-03-04 22:17:04 -0300

    Creating Function Aws Lambda

---
+++ WebAdvert.SearchWorker/WebAdvert.SearchWorker/SearchWorker.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.Lambda.Core;
+using Amazon.Lambda.SNSEvents;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+namespace WebAdvert.SearchWorker
+{
+    public class SearchWorker
+    {
+        public void Function(SNSEvent snsEvent, ILambdaContext context)
+        {
+            foreach(var record in snsEvent.Records)
+            {
+                context.Logger.LogLine(record.Sns.Message);
+            }
+        }
+    }
+}
